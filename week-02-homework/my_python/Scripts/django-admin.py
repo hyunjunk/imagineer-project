5f67e05cb3c5cf9b39c974cca95c7353b1fe1a38
HyunjunK <43165776+HyunjunK@users.noreply.github.com>
2018-11-06 23:52:46 +0900

    update after second week

---
+++ week-02-homework/my_python/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\hyunj\imagineer-project\week-02-homework\my_python\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
